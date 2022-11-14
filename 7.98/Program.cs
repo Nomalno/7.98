1f441054d6b67744b9ca24de6d7ac22c17fa2ca3
Горячев Владимир Сергеевич <51385107+Nomalno@users.noreply.github.com>
2022-11-14 20:36:31 +0300

    Update Program.cs

--- 7.98/Program.cs
+++ 7.98/Program.cs
@@ -1,6 +1,8 @@
-﻿{
+{
     double n = 38.8;
     double p = 81.3;
+    Console.WriteLine("Число p={0}",p);
+    Console.WriteLine("Массив:");
     Random nums = new Random();
     double[] array = new double[10];
     for (int i = 0; i < array.Length; i++)
@@ -8,7 +10,7 @@
         array[i] = nums.NextDouble() * 16.1;
         Console.WriteLine(array[i].ToString("F1"));
     }
-        double x = Enumerable.Sum(array);
+        double x = Enumerable.Sum(array)+n;
         Console.WriteLine("Сумма чисел в массиве={0}", x.ToString("F1"));
     if (p <x)
     {
